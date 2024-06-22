62270829b5eec57032bb98317fc56ab4632d4608
Hamza Zaafrane <zaafranehamza@gmail.com>
2024-06-22 16:16:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0551ce4d67096d606"  # Replace with your desired AMI
+  ami           = "ami-0b17db95571fd002c"  # Replace with your desired AMI
   instance_type = "t2.micro"
 
   tags = {
